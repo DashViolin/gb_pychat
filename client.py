75dd2e33b62830902cfda3518086b88c0e4fad56
DashViolin <mymail@srv.org>
2022-10-17 18:04:59 +0300

    lesson01 - str, bytes, encoding and decoding - fix issues

--- client.py
+++ client.py
@@ -1,24 +1,24 @@
+import chardet
+
 print("Задание 1:")
 # 1. Каждое из слов "разработка", "сокет", "декоратор" представить в строковом формате и проверить тип и содержание соответствующих переменных.
 # Затем с помощью онлайн-конвертера преобразовать строковые представление в формат Unicode и также проверить тип и содержимое переменных.
 
-a = "разработка"
-b = "сокет"
-c = "декоратор"
-
-for var in (a, b, c):
+for var in ("разработка", "сокет", "декоратор"):
     print(type(var), var, len(var), sep="\n", end="\n")
-
+print()
+for var in (
+    "\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430",
+    "\u0441\u043e\u043a\u0435\u0442",
+    "\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440",
+):
+    print(type(var), var, len(var), sep="\n", end="\n")
 
 print("\nЗадание 2:")
 # 2. Каждое из слов "class", "function", "method" записать в байтовом типе без преобразования в последовательность кодов
 # (не используя методы encode и decode) и определить тип, содержимое и длину соответствующих переменных.
 
-a = b"class"
-b = b"function"
-c = b"method"
-
-for var in (a, b, c):
+for var in (b"class", b"function", b"method"):
     print(type(var), var, len(var), sep="\n", end="\n")
 
 
@@ -49,7 +49,9 @@ for site in ("yandex.ru", "youtube.com"):
     args = ["ping", "-c", "4", site]
     ping = subprocess.Popen(args, stdout=subprocess.PIPE)
     for line in ping.stdout:
-        print(line.decode("cp866").strip())
+        encoding = chardet.detect(line)['encoding']
+        print(line.decode(encoding).strip())
+    print(f'Кодировка: {encoding}')
 
 
 print("\nЗадание 6:")
@@ -67,6 +69,10 @@ data = ("сетевое программирование", "сокет", "дек
 with open(file, "w", encoding=default_coding) as f:
     for word in data:
         f.write(f"{word}\n")
-with open(file, "r", encoding="utf8") as f:
-    print(f.read())
+with open(file, "rb") as f:
+    raw = f.read()
+    encoding = chardet.detect(raw)['encoding']    
+    print(f'Кодировка файла: {encoding}')
+    text = raw.decode(encoding)
+    print(text)
 os.remove(file)
